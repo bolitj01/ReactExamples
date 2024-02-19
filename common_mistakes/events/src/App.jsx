33add28045c3340ea13f37d57e8daf4b48fab73d
Thomas Bolinger <bolitj01@pfw.edu>
2024-02-18 23:33:24 -0500

    Improved contact exercise for best practice

--- common_mistakes/events/src/App.jsx
+++ common_mistakes/events/src/App.jsx
@@ -3,26 +3,26 @@ import './App.css';
 
 function App() {
 
-  const [message, setMessage] = useState(0);
+  const [message, setMessage] = useState("Hi");
 
   let showMessage = () => {
     // return "Surprise!";
-    // document.querySelector("h3").innerHTML = "Surprise!";
-    // setMessage("Surprise!");
+    document.querySelector("h3").innerHTML = "Surprise!";
+    setMessage("Hi!");
   }
 
   let hideMessage = () => {
     // return "";
-    // document.querySelector("h3").innerHTML = "";
-    // setMessage("");
+    document.querySelector("h3").innerHTML = "";
+    setMessage("");
   }
 
   return (
     <>
       <h1>Toggle Message</h1>
       <h3>{message}</h3>
-      <button onClick={showMessage()}>Show</button>
-      <button onClick={hideMessage()}>Hide</button>
+      <button onClick={() => showMessage()}>Show</button>
+      <button onClick={() => hideMessage()}>Hide</button>
       {/* onClick expects a callback */}
     </>
   )
--- exercises/contact_list/src/components/ViewContacts.jsx
+++ exercises/contact_list/src/components/ViewContacts.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Contact from './Contact';
+import AddContact from './AddContact';
+import { v4 } from 'uuid';
 
-const ViewContacts = ({ title, contacts }) => {
+const ViewContacts = ({ title }) => {
 
-    // props.contacts
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        setContacts(() => {
+            //Test data
+            return [
+                {
+                    id: v4(),
+                    name: "Tommy",
+                    phone: "555-555-5555"
+                },
+                {
+                    id: v4(),
+                    name: "Haley",
+                    phone: "444-444-4444"
+                }
+            ]
+        });
+    }, [])
+
+    //Incomplete functions to manage the contacts
+    const removeContact = () => {
+        
+    }
+
+    const addContact = () => {
+        
+    }
 
     return (
         <>
+            <AddContact ></AddContact>
             <h1>{title}</h1>
             {contacts.map((c) => {
                 return (
-                    <>
-                        <span>{c.name}</span>
-                        <span>{c.phone}</span>
-                        <span>Delete</span>
-                    </>
+                    <Contact />
                 )
             })}
         </>
